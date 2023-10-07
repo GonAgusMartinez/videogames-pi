1d1c2acc18edfbc3f77dc2c3dc53ee46a87bff1c
Gonzalo.A.Mart <cuentasdechalo@gmail.com>
2023-10-07 10:26:13 -0300

    Dia 1

---
+++ PI-Videogames-main/api/src/models/Genre.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Genre = sequelize.define('Genre', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Genre;
+};
